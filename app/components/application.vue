ce72c9affd9d05fab9b04b41bb0327e254441074
John Michael McCasland <jmmccasland@gmail.com>
2017-03-06 13:18:06 -0600

    Header markup and style

---
+++ app/components/application.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="app">
+    <!-- HEADER -->
+    <div class="section section--header">
+      <div class="container">
+        <h1 class="logo">Marvel</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
