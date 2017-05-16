dfb9c3b76f24cd2e56c318feff1dd3363982d44e
John Michael McCasland <jmmccasland@gmail.com>
2017-05-16 16:19:21 -0500

    Viewport meta tag

--- app/actions.js
+++ app/actions.js
@@ -36,7 +36,7 @@ export function modalClear(data) {
 
 export function charactersFindForId(id) {
   return (dispatch) => {
-    fetch(`http://marvel-is-broke.herokuapp.com/series/${id}/characters`)
+    fetch(`https://gateway.marvel.com:443/v1/public/series/${id}/characters?orderBy=-name&apikey=576ba112089165ff4de72eda3db860ec`)
     .then(res => res.json())
     .then((data) => {
       const characters = data.data.results;
@@ -47,7 +47,7 @@ export function charactersFindForId(id) {
 
 export function issuesFindForId(id) {
   return (dispatch) => {
-    fetch(`http://marvel-is-broke.herokuapp.com/series/${id}/comics`)
+    fetch(`https://gateway.marvel.com:443/v1/public/series/${id}/comics?apikey=576ba112089165ff4de72eda3db860ec`)
     .then(res => res.json())
     .then((data) => {
       const issues = data.data.results;
@@ -58,7 +58,10 @@ export function issuesFindForId(id) {
 
 export function seriesInfoSearch(name) {
   return (dispatch) => {
-    fetch(`http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=${name}`)
+    // fetch(`http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=${name}`)
+    fetch(`https://gateway.marvel.com:443/v1/public/series?titleStartsWith=${name}&apikey=576ba112089165ff4de72eda3db860ec
+`)
+    // fetch(`https://gateway.marvel.com:443/v1/public/series?limit=1&titleStartsWith=${name}/&apikey=576ba112089165ff4de72eda3db860ec`)
     .then(res => res.json())
     .then((data) => {
       const series = data.data.results[0];
